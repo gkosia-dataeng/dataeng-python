775a31f1d9656da89813a3f2320053cdf6db19e4
gkosia-pps <gavrilis.kosiari@pepperstone.com>
2026-02-25 12:45:58 +0200

    move production-ready-engineering to big-data-engineer report

--- production_ready_engineering/src/capstone/config/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Config(BaseSettings):
-    LOG_LEVEL: str
-    DATA_PATH: str
-    DLQ_PATH: str
-    DB_PATH: str
-    model_config = SettingsConfigDict(env_file= './.env')
--- production_ready_engineering/src/capstone/models/events.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Event(BaseModel):
-    event_id: str
-    user_id: int
-    event_type: str
-    amount: float
-    timestamp: str
